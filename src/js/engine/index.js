cecc9892963284cde6fc87c59b66c79e2a436721
Niels Gerritsen <n.c.gerritsen@gmail.com>
2020-09-06 20:25:36 +0200

    Cache last result

--- src/js/engine/index.js
+++ src/js/engine/index.js
@@ -2,22 +2,29 @@ import tokenize from './tokenize';
 import parse from './parse';
 import evaluate from './evaluate';
 
+let last = null;
+
 export function execute(code = '') {
+  if (last && code === last.code) {
+    return last;
+  }
+
   if (code.trim() === '') {
-    return {
-      result: 0,
-    };
+    return createResult(code);
   }
 
   try {
-    return {
-      result: evaluate(parse(tokenize(code))),
-    };
+    const result = evaluate(parse(tokenize(code)));
+    last = createResult(code, null, result);
   } catch (error) {
-    return {
-      error,
-    };
+    last = createResult(code, error);
   }
+
+  return last;
+}
+
+function createResult(code, error = null, result = 0) {
+  return { code, error, result };
 }
 
 export function isAllowed(char) {
